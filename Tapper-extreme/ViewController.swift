9f260fe256a4f463b836d79efbf01965325065c8
David Quaschnick <quaschnick@hotmail.com>
2016-05-07 22:17:45 -0700

    Initial Commit

---
+++ Tapper-extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper-extreme
+//
+//  Created by David Quaschnick on 5/7/16.
+//  Copyright © 2016 quaschnick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
